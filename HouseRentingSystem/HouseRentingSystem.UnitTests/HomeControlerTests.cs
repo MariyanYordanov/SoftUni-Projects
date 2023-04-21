2d8639870c2e8f7108e953085c8e264e8f973ee0
Mariyan Yordanov <marsrewq@gmail.com>
2023-04-21 18:03:56 +0300

    Add NUnitTests Project

---
+++ HouseRentingSystem/HouseRentingSystem.UnitTests/HomeControlerTests.cs
@@ -0,0 +1,16 @@
+namespace HouseRentingSystem.UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Home_Controler_Tests_Constructor()
+        {
+            Assert.Pass();
+        }
+    }
+}
